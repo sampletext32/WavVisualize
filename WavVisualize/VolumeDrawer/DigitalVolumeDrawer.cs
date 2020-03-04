accd27d50dfb75601b5ea435e630e3f4f0e9421f
sampletext32 <sampletext32@bk.ru>
2020-03-04 18:39:02 +0300

    DigitalVolumeProvider Calculations Optimization

--- WavVisualize/VolumeDrawer/DigitalVolumeDrawer.cs
+++ WavVisualize/VolumeDrawer/DigitalVolumeDrawer.cs
@@ -24,11 +24,14 @@ namespace WavVisualize
             int digitalPartsL = (int) (NormalizedVolumeL * _bandPieces);
             int digitalPartsR = (int) (NormalizedVolumeR * _bandPieces);
 
+            var pieceWidthSpaceHeight = _bandPieceHeight + _distanceBetweenPieces;
+            var firstPieceY = DisplayRectangle.Bottom - _bandPieceHeight;
+
             //рисуем цифровые части левой громкости
             for (int i = 0; i < digitalPartsL; i++)
             {
                 g.FillRectangle(LeftBrush, DisplayRectangle.Left,
-                    DisplayRectangle.Bottom - i * (_bandPieceHeight + _distanceBetweenPieces) - _bandPieceHeight,
+                    firstPieceY - i * pieceWidthSpaceHeight,
                     DisplayRectangle.CenterW, _bandPieceHeight);
             }
 
@@ -36,7 +39,7 @@ namespace WavVisualize
             for (int i = 0; i < digitalPartsR; i++)
             {
                 g.FillRectangle(RightBrush, DisplayRectangle.CenterW,
-                    DisplayRectangle.Bottom - i * (_bandPieceHeight + _distanceBetweenPieces) - _bandPieceHeight,
+                    firstPieceY - i * pieceWidthSpaceHeight,
                     DisplayRectangle.CenterW, _bandPieceHeight);
             }
         }
