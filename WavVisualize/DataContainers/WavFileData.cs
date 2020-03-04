91d76e9ec952416a531d9f40164afe0029557169
sampletext32 <sampletext32@bk.ru>
2020-03-04 19:10:54 +0300

    WavFile General Refactor

--- WavVisualize/DataContainers/WavFileData.cs
+++ WavVisualize/DataContainers/WavFileData.cs
@@ -8,26 +8,58 @@ namespace WavVisualize
     {
         #region Параметры Wav файла
 
-        public int ChunkId;
-        public int FileSize;
-        public int RiffType;
-        public int FmtId;
-        public int FmtSize;
-        public int FmtExtraSize;
-        public int FmtCode;
-        public int Channels;
-        public int SampleRate;
-        public int ByteRate;
-        public int FmtBlockAlign;
-        public int BitDepth;
-        public int DataId;
-        public int BytesLength;
-        public int BytesForSample;
-        public int SamplesCount;
+        //Done, using https://audiocoding.ru/articles/2008-05-22-wav-file-structure/
+
+        //TODO: ENCAPSULATION FIX !IMPORTANT
+
+        /*0  -  3*/ /* 4 */
+        public String chunkId; //содержит символы "RIFF"
+
+        /*4  -  7*/ /* 4 */
+        public int chunkSize; //размер оставшейся цепочки - размер файла минус 8 байт
+
+        /*8  - 11*/ /* 4 */
+        public String format; //содержит символы "WAVE"
+
+        /*12 - 15*/ /* 4 */
+        public String subchunk1Id; //содержит символы "fmt "
+
+        /*16 - 19*/ /* 4 */
+        public int subchunk1Size; //16 для PCM - размер оставшейся подцепочки
+
+        /*20 - 21*/ /* 2 */
+        public short
+            audioFormat; //1 для PCM TODO: extend https://audiocoding.ru/articles/2008-05-22-wav-file-structure/wav_formats.txt
+
+        /*22 - 23*/ /* 2 */
+        public short numChannels; //количество каналов
+
+        /*24 - 27*/ /* 4 */
+        public int sampleRate; //частота дискретизации
+
+        /*28 - 31*/ /* 4 */
+        public int byteRate; //количество байт в секунду воспроизведения
+
+        /*32 - 33*/ /* 2 */
+        public short blockAlign; //байт на 1 сэмпл включая все каналы
+
+        /*34 - 35*/ /* 2 */
+        public short bitsPerSample; //количество бит в сэмпле или глубина кодирования
+
+        /*36 - 39*/ /* 4 */
+        public String subchunk2Id; //содержит символы "data"
+
+        /*40 - 43*/ /* 4 */
+        public int subchunk2Size; //количество байт в области данных
+
+        /*44 -inf*/ /* 0 */
+        byte[] data; //сами WAV данные
 
         #endregion
 
         #region Используемые для вычислений данные
+
+        public float[] RawSamples;
 
         public float[] LeftChannel;
         public float[] RightChannel;
@@ -43,118 +75,127 @@ namespace WavVisualize
             return spectrum;
         }
 
-        //функция читает Wav файл из потока
-        public static WavFileData ReadWav(Stream stream)
+        private float[] ExtractSamples(byte[] data)
         {
-            WavFileData wavFileData = new WavFileData();
-            try
+            int length = data.Length;
+            float[] samples;
+            //TODO: Increase performance by excluding every step POW calculation
+            switch (bitsPerSample)
             {
-                using (stream)
+                case 8:
+                    samples = new float[length];
+                    for (int i = 0; i < samples.Length; i++)
+                    {
+                        int sample = (int) (data[i * 1 + 0] & 0xff);
+                        samples[i] = (float) (sample / Math.Pow(2, 8));
+                    }
+
+                    break;
+                case 16:
+                    samples = new float[length / 2];
+                    for (int i = 0; i < samples.Length; i++)
+                    {
+                        short sample = (short) (
+                            ((data[i * 2 + 0] & 0xff) << 0) | ((data[i * 2 + 1] & 0xff) << 8));
+                        samples[i] = (float) (sample / Math.Pow(2, 16));
+                    }
+
+                    break;
+                case 24:
+                    samples = new float[length / 3];
+                    for (int i = 0; i < samples.Length; i++)
+                    {
+                        //т.к. 24 бит влезает только в 32 бит число, записываем как есть и смещаем всё на 8 бит влево.
+                        int sample
+                            = (((data[i * 3 + 0] & 0xff) << 0) | ((data[i * 3 + 1] & 0xff) << 8) |
+                               ((data[i * 3 + 2] & 0xff) << 16)) << 8;
+                        samples[i] =
+                            (float) (sample / Math.Pow(2,
+                                         32)); //Делим на 2^32 - максимальное число в 32 битном числе, а не 24.
+                    }
+
+                    break;
+                case 32:
+                    samples = new float[length / 4];
+                    for (int i = 0; i < samples.Length; i++)
+                    {
+                        int sample
+                            = ((data[i * 4 + 0] & 0xff) << 0) | ((data[i * 4 + 1] & 0xff) << 8) |
+                              ((data[i * 4 + 2] & 0xff) << 16) | ((data[i * 4 + 3] & 0xff) << 24);
+                        samples[i] = (float) (sample / Math.Pow(2, 32));
+                    }
+
+                    break;
+                case 64:
+                    samples = new float[length / 8];
+                    for (int i = 0; i < samples.Length; i++)
+                    {
+                        long sample =
+                            ((long) (data[i * 8 + 0] & 0xff) << 0) | ((long) (data[i * 8 + 1] & 0xff) << 8) |
+                            ((long) (data[i * 8 + 2] & 0xff) << 16) | ((long) (data[i * 8 + 3] & 0xff) << 24) |
+                            ((long) (data[i * 8 + 4] & 0xff) << 32) | ((long) (data[i * 8 + 5] & 0xff) << 40) |
+                            ((long) (data[i * 8 + 6] & 0xff) << 48) | ((long) (data[i * 8 + 7] & 0xff) << 56);
+                        samples[i] = (float) (sample / Math.Pow(2, 64));
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Unknown BitDepth");
+            }
+
+            return samples;
+        }
+
+        public WavFileData(byte[] fileData)
+        {
+            using (var ms = new MemoryStream(fileData))
+            {
+                BinaryReader reader = new BinaryReader(ms);
+
+                chunkId = "" + (char) reader.ReadByte() + (char) reader.ReadByte() + (char) reader.ReadByte() +
+                          (char) reader.ReadByte(); // 0x52494646
+                chunkSize = reader.ReadInt32();
+                format = "" + (char) reader.ReadByte() + (char) reader.ReadByte() + (char) reader.ReadByte() +
+                         (char) reader.ReadByte(); // 0x57415645
+                subchunk1Id = "" + (char) reader.ReadByte() + (char) reader.ReadByte() + (char) reader.ReadByte() +
+                              (char) reader.ReadByte(); // 0x666d7420
+                subchunk1Size = reader.ReadInt32();
+                audioFormat = reader.ReadInt16();
+                numChannels = reader.ReadInt16();
+                sampleRate = reader.ReadInt32();
+                byteRate = reader.ReadInt32();
+                blockAlign = reader.ReadInt16();
+                bitsPerSample = reader.ReadInt16();
+                subchunk2Id = "" + (char) reader.ReadByte() + (char) reader.ReadByte() + (char) reader.ReadByte() +
+                              (char) reader.ReadByte(); // 0x64617461
+                subchunk2Size = reader.ReadInt32();
+                data = reader.ReadBytes((int) (ms.Length - ms.Position));
+
+                RawSamples = ExtractSamples(data);
+
+                switch (numChannels)
                 {
-                    BinaryReader reader = new BinaryReader(stream);
+                    case 1:
+                        //если записано МОНО, оба канала = исходному
+                        LeftChannel = RawSamples;
+                        RightChannel = RawSamples;
+                        break;
+                    case 2:
+                        //если записано СТЕРЕО
+                        //создаём 2 массива на левый и правый канал с количеством сэмплов
+                        LeftChannel = new float[RawSamples.Length / numChannels];
+                        RightChannel = new float[RawSamples.Length / numChannels];
+                        for (int i = 0, s = 0; i < RawSamples.Length / numChannels; i++)
+                        {
+                            //записываем сэмплы
+                            LeftChannel[i] = RawSamples[s++];
+                            RightChannel[i] = RawSamples[s++];
+                        }
 
-                    wavFileData.ChunkId = reader.ReadInt32();
-                    wavFileData.FileSize = reader.ReadInt32();
-                    wavFileData.RiffType = reader.ReadInt32();
-
-                    wavFileData.FmtId = reader.ReadInt32();
-                    wavFileData.FmtSize = reader.ReadInt32();
-                    wavFileData.FmtCode = reader.ReadInt16();
-                    wavFileData.Channels = reader.ReadInt16();
-                    wavFileData.SampleRate = reader.ReadInt32();
-                    wavFileData.ByteRate = reader.ReadInt32();
-                    wavFileData.FmtBlockAlign = reader.ReadInt16();
-                    wavFileData.BitDepth = reader.ReadInt16();
-
-                    if (wavFileData.FmtSize == 18)
-                    {
-                        wavFileData.FmtExtraSize = reader.ReadInt16();
-                        reader.ReadBytes(wavFileData.FmtExtraSize);
-                    }
-
-                    wavFileData.DataId = reader.ReadInt32();
-                    wavFileData.BytesLength = reader.ReadInt32();
-
-                    byte[] data = reader.ReadBytes(wavFileData.BytesLength);
-
-                    wavFileData.BytesForSample =
-                        wavFileData.BitDepth / 8; //байт на сэмпл - (глубина кодирования в битах) / 8(бит в байте)
-
-                    //количество сэмплов =  общее_количество_байт   / байт_на_сэмпл/ количество_каналов
-                    //                      Сумма                   / Скорость      / Параллельность
-                    wavFileData.SamplesCount =
-                        wavFileData.BytesLength / wavFileData.BytesForSample / wavFileData.Channels;
-
-                    float[] asFloat;
-                    switch (wavFileData.BitDepth)
-                    {
-                        case 64:
-                            //количество значений = количество_сэмплов * количество_каналов
-                            var asDouble = new double[wavFileData.SamplesCount * wavFileData.Channels];
-                            Buffer.BlockCopy(data, 0, asDouble, 0, wavFileData.BytesLength);
-                            asFloat = Array.ConvertAll(asDouble, e => (float) e);
-                            break;
-                        case 32:
-                            //количество значений = количество_сэмплов * количество_каналов
-                            asFloat = new float[wavFileData.SamplesCount * wavFileData.Channels];
-                            Buffer.BlockCopy(data, 0, asFloat, 0, wavFileData.BytesLength);
-                            break;
-                        case 16:
-                            //количество значений = количество_сэмплов * количество_каналов
-                            var asInt16 = new short[wavFileData.SamplesCount * wavFileData.Channels];
-                            Buffer.BlockCopy(data, 0, asInt16, 0, wavFileData.BytesLength);
-                            asFloat = Array.ConvertAll(asInt16,
-                                e => e / (float) short.MaxValue); //нормализуем, деля на максимальное значение
-                            break;
-                        default: throw new FormatException("Unknown BitDepth");
-                    }
-
-                    switch (wavFileData.Channels)
-                    {
-                        case 1:
-                            //если записано МОНО, оба канала = исходному
-                            wavFileData.LeftChannel = asFloat;
-                            wavFileData.RightChannel = asFloat;
-                            break;
-                        case 2:
-                            //если записано СТЕРЕО
-                            //создаём 2 массива на левый и правый канал с количеством сэмплов
-                            wavFileData.LeftChannel = new float[wavFileData.SamplesCount];
-                            wavFileData.RightChannel = new float[wavFileData.SamplesCount];
-                            for (int i = 0, s = 0; i < wavFileData.SamplesCount; i++)
-                            {
-                                //записываем сэмплы
-                                wavFileData.LeftChannel[i] = asFloat[s++];
-                                wavFileData.RightChannel[i] = asFloat[s++];
-                            }
-
-                            break;
-                        default: throw new FormatException("Unknown Channels");
-                    }
+                        break;
+                    default: throw new FormatException("Unknown Channels");
                 }
             }
-            catch (Exception ex)
-            {
-                return null;
-            }
-
-            return wavFileData;
-        }
-
-        //функция читает Wav файл из массива байт
-        public static WavFileData ReadWav(byte[] stream)
-        {
-            WavFileData wavFileData = ReadWav(new MemoryStream(stream));
-
-            return wavFileData;
-        }
-
-        //функция читает Wav файл, открывая файл по переданному пути
-        public static WavFileData ReadWav(string filename)
-        {
-            WavFileData wavFileData = ReadWav(File.OpenRead(filename));
-
-            return wavFileData;
         }
     }
 }
