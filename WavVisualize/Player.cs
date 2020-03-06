4a8623d9041aab62b4e0f8d3094335a1c485f364
sampletext32 <sampletext32@bk.ru>
2020-03-06 22:16:17 +0300

    Add Player Class

---
+++ WavVisualize/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace WavVisualize
+{
+    public class Player
+    {
+        private WindowsMediaPlayerProvider _playerProvider;
+
+        private WavFileData _currentWavFileData;
+
+        private WaveformProvider _waveformProvider;
+
+        private FFTProvider _fftProvider;
+
+        private VolumeProvider _volumeProvider;
+
+        private VolumeDrawer _volumeDrawer;
+
+        private SpectrumDrawer _spectrumDrawer;
+
+        private SpectrumDiagram _spectrumDiagram;
+        
+        //Settings _settings;
+
+        public Player(/* Settings settings */)
+        {
+
+        }
+    }
+}
