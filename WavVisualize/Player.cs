48727fd6f629e2994b4a3ef979d6633bdfd3945d
sampletext32 <sampletext32@bk.ru>
2020-03-06 23:03:26 +0300

    Weird Shit Commit

---
+++ WavVisualize/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+namespace WavVisualize
+{
+    public class Player
+    {
+        private WindowsMediaPlayerProvider _playerProvider;
+
+        private WavFileData _currentWavFileData;
+
+        private WaveformProvider _waveformProvider;
+
+        private VolumeProvider _volumeProvider;
+
+        private FFTProvider _fftProvider;
+
+        private VolumeDrawer _volumeDrawer;
+
+        private SpectrumDrawer _spectrumDrawer;
+
+        private SpectrumDiagram _spectrumDiagram;
+        
+        private Settings _settings;
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnDraw(Graphics waveformGraphics, Graphics spectrumDiagramGraphics, Graphics volumeGraphics, Graphics spectrumGraphics)
+        {
+            //Draw Waveform
+            //Draw Spectrum Diagram
+            //Draw Volume
+            //Draw Spectrum
+        }
+
+        public Player(Settings settings)
+        {
+            _settings = settings;
+        }
+    }
+}
---
+++ WavVisualize/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WavVisualize
+{
+    public class Settings
+    {
+        public PlayerSettings PlayerSettings { get; }
+        public WaveformSettings WaveformSettings { get; }
+        public SpectrumSettings SpectrumSettings { get; }
+        public VolumeSettings VolumeSettings { get; }
+
+        public Settings(PlayerSettings playerSettings, WaveformSettings waveformSettings,
+            SpectrumSettings spectrumSettings, VolumeSettings volumeSettings)
+        {
+            PlayerSettings = playerSettings;
+            WaveformSettings = waveformSettings;
+            SpectrumSettings = spectrumSettings;
+            VolumeSettings = volumeSettings;
+        }
+
+        public static Settings Default = new Settings(
+            new PlayerSettings(60, 4096, 20000, 0.1f, true),
+            new WaveformSettings(true, Environment.ProcessorCount - 1, 0, 1),
+            new SpectrumSettings(2, 1, false, 100),
+            new VolumeSettings(2, 1)
+        );
+    }
+}
