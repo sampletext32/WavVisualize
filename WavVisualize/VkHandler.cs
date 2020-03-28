3f26a6d0cfac006ad32b454714de95fbd06ff1f6
sampletext32 <sampletext32@bk.ru>
2020-03-28 21:35:02 +0300

    Improve GetAudios

--- WavVisualize/VkHandler.cs
+++ WavVisualize/VkHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,18 +8,56 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using VkNet;
 using VkNet.AudioBypassService.Extensions;
+using VkNet.Model;
+using VkNet.Model.Attachments;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
 
 namespace WavVisualize
 {
     public class VkHandler
     {
-        public void Login()
+        private VkApi _api;
+
+        public static VkHandler Login(string login, string password)
         {
             var services = new ServiceCollection();
 
             services.AddAudioBypass();
 
             var api = new VkApi(services);
+            api.Authorize(new ApiAuthParams
+            {
+                Login = login,
+                Password = password,
+                TwoFactorAuthorization = () =>
+                {
+                    Console.WriteLine(" > Код двухфакторной аутентификации:");
+                    return Console.ReadLine();
+                }
+            });
+            return new VkHandler(api);
+        }
+
+        public void GetAudios()
+        {
+            if (_api.UserId == null)
+            {
+                Debug.WriteLine("UserID null! in GetAudios");
+                return;
+            }
+            VkCollection<Audio> audios = _api.Audio.Get(new AudioGetParams { Count = _api.Audio.GetCount((long) _api.UserId) });
+            foreach (var audio in audios)
+            {
+                Debug.WriteLine($" > {audio.Artist} - {audio.Title} - {audio?.Url}");
+            }
+
+            //audios.ToList();
+        }
+
+        private VkHandler(VkApi api)
+        {
+            _api = api;
         }
     }
 }
