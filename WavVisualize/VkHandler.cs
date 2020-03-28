ef8e05a7e13c4a36058595ddac201c8b346a3141
sampletext32 <sampletext32@bk.ru>
2020-03-28 17:15:44 +0300

    Hard add VKNET and VKNET.AudioBypass Upgrade to 4.6.1 Test add VkHandler

---
+++ WavVisualize/VkHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using VkNet;
+using VkNet.AudioBypassService.Extensions;
+
+namespace WavVisualize
+{
+    public class VkHandler
+    {
+        public void Login()
+        {
+            var services = new ServiceCollection();
+
+            services.AddAudioBypass();
+
+            var api = new VkApi(services);
+        }
+    }
+}
