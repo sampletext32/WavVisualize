55b5085b422806bdc9ba16b5c543cb609045bbd8
sampletext32 <sampletext32@bk.ru>
2020-03-31 23:44:13 +0300

    Add ConvertM3U8ToMp3Url

--- WavVisualize/VkHandler.cs
+++ WavVisualize/VkHandler.cs
@@ -46,7 +46,9 @@ namespace WavVisualize
                 Debug.WriteLine("UserID null! in GetAudios");
                 return;
             }
-            VkCollection<Audio> audios = _api.Audio.Get(new AudioGetParams { Count = _api.Audio.GetCount((long) _api.UserId) });
+
+            VkCollection<Audio> audios = _api.Audio.Get(new AudioGetParams
+                {Count = _api.Audio.GetCount((long) _api.UserId)});
             foreach (var audio in audios)
             {
                 Debug.WriteLine($" > {audio.Artist} - {audio.Title} - {audio?.Url}");
@@ -62,11 +64,43 @@ namespace WavVisualize
                 Debug.WriteLine("UserID null! in GetAudios");
                 return "";
             }
-            VkCollection<Audio> audios = _api.Audio.Get(new AudioGetParams { Count = 1 });
+
+            VkCollection<Audio> audios = _api.Audio.Get(new AudioGetParams {Count = 1});
 
             return audios[0].Url.ToString();
 
             //audios.ToList();
+        }
+
+        public static string ConvertM3U8ToMp3Url(string m3u8Url)
+        {
+            //M3U8 - https://
+            //cs1-60v4.vkuseraudio.net/
+            //p5/
+            //76ce3802735/
+            //ec33d12145c3df/
+            //index.m3u8
+            //?extra=Ik4FLqrrtzGIypVVRwCVLgbX9fT2g34_oCNO-BGfu9apU7tyGra5kUVtKucUWvaXsC0czWqlxUndFq5DCoOOL4RkYKnVErsLy0gxwmn31M4fOWYQ1GyRWFKHULzLxf-I1FIx05WR-KIvcn1MuMiOOPQ
+
+            //MP3 - https://
+            //cs1-60v4.vkuseraudio.net/
+            //p5/
+            //ec33d12145c3df.mp3
+            //?extra=Ik4FLqrrtzGIypVVRwCVLgbX9fT2g34_oCNO-BGfu9apU7tyGra5kUVtKucUWvaXsC0czWqlxUndFq5DCoOOL4RkYKnVErsLy0gxwmn31M4fOWYQ1GyRWFKHULzLxf-I1FIx05WR-KIvcn1MuMiOOPQ
+
+            string extra = m3u8Url.Remove(0, m3u8Url.IndexOf("?extra"));
+            
+            //all that is not extra
+            string mainUrl = m3u8Url.Substring(0, m3u8Url.IndexOf("?extra"));
+            
+            //trim https://
+            mainUrl = mainUrl.Remove(0, 8);
+
+            string[] tokens = mainUrl.Split('/');
+
+            string url = "https://" + tokens[0] + "/" + tokens[1] + "/" + tokens[3] + ".mp3" + extra;
+
+            return url;
         }
 
         private VkHandler(VkApi api)
