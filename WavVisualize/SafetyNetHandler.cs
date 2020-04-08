a290c88e6588755194614ff0b106051aa69cbc1e
sampletext32 <sampletext32@bk.ru>
2020-04-08 23:14:32 +0300

    SafetyNet Faker  work

---
+++ WavVisualize/SafetyNetHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WavVisualize
+{
+    public class SafetyNetHandler
+    {
+        private const string GcmUserAgent = "Android-GCM/1.5 (generic_x86 KK)";
+    }
+}
